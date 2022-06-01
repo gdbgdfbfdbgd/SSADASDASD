19dd64514d2c9cb4afb51695efcb984a60c36086
gdbgdfbfdbgd <106006283+gdbgdfbfdbgd@users.noreply.github.com>
2022-06-01 16:44:07 +0700

    Add files via upload

---
+++ FILE_20220521_175859_tt.py
@@ -0,0 +1,8 @@
+import os , sys
+from time import sleep
+
+ngày = 1
+while True:
+	print ("\033[1;32mI Tool \033[1;31m| \033[1;36m1 | tao thick mày I love huyền♡ " + str(ngày) )
+	ngày += 1
+	sleep(0.2)
